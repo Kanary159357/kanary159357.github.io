bd5708b8dc5f65162b73a5f047b0a81608971d1d
kanary <kanary@adminui-MacBookPro.local>
2024-01-28 20:56:32 +0900

    update shaku setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import { remarkShakuCodeAnnotate } from "remark-shaku-code-annotate";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "kanary159357.github.io",
+  site: "https://kanary159357.github.io",
   markdown: {
     remarkPlugins: [
       [
